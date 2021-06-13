4f1507d0c590f75a2a1921c0049b30d84e4524c0
Farhan71 <farhanitnstu@gmail.com>
2021-06-14 02:18:23 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import './App.css';
+import LIneChart from './Components/LineChart/LIneChart';
+import News from './Components/News/News';
+import Piechart from './Components/Piechart/Piechart';
 
 function App() {
+  const [articles, setArticles] = useState([])
+  // useEffect(()=>{
+  //     fetch ('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bee05a5023a74e43aa05de6f147f2970')
+  //     .then ( res => res.json() )
+  //     .then (data => setArticles(data.articles))
+  // },[])
+  useEffect(()=>{
+    const url ='https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bee05a5023a74e43aa05de6f147f2970'
+    axios(url)
+    .then (data => setArticles(data.data.articles))
+},[])
+  const newsStyle = {
+    // float: 'left',
+    // margin: '10px',
+    marginRight: '30px',
+    display: 'flex',
+    flexWrap: "wrap",
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Button color="primary">Hello World</Button> <br />
+      <Button variant="contained" color="secondary">Secondary</Button>
+      <h1>Total articles: {articles.length}</h1>
+      <div style={newsStyle}>
+        {
+          articles.map (article => <News article={article}></News>  ) 
+          
+        }
+      </div>
+      <LIneChart></LIneChart>
+      <Piechart></Piechart>
+      
     </div>
   );
 }
